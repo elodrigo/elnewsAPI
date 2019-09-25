297d9d643e52ec3898635dceb29b009b74fab020
elodrigo <elodrigo@daum.net>
2019-09-25 18:39:17 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"elnewsAPI/api"
+	"elnewsAPI/loads"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	Port = ":8181"
+	Key = "key_here"
+	Cert = "cert_here"
+)
+
+func init() {
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	loads.LoadConfiguration()
+}
+
+
+func main() {
+	var allowedOrigins []string
+
+	if loads.ElDevelopment {
+		allowedOrigins = []string{"https://elnewspaper.com", "https://www.elnewspaper.com", "http://localhost:8080", "http://127.0.0.1:8080"}
+	} else {
+		allowedOrigins = []string{"https://elnewspaper.com", "https://www.elnewspaper.com"}
+	}
+
+	r := mux.NewRouter()
+
+	// Events SubRouter
+	eventsRouter := r.PathPrefix("/events").Subrouter()
+
+	eventsRouter.Methods("GET").Path("/first_posts_json").HandlerFunc(api.PostsUnionJSON)
+	eventsRouter.Methods("GET").Path("/next_posts_json").HandlerFunc(api.PostsUnionJSON)
+	eventsRouter.Methods("GET").Path("/saying_today_array").HandlerFunc(api.SayingTodayJSONArray)
+	eventsRouter.Methods("POST").Path("/weather_today_normal").HandlerFunc(api.WeatherDongNeTodayJSON)
+
+	// Writer SubRouter
+	writerRouter := r.PathPrefix("/writer").Subrouter()
+
+	// Writing Events by Editors
+	writerRouter.Methods("POST").Path("/insert_writing/cheditor").HandlerFunc(api.InsertChEditor)
+	writerRouter.Methods("POST").Path("/update_writing/cheditor").HandlerFunc(api.UpdateChEditor)
+	writerRouter.Methods("POST").Path("/delete_writing/cheditor").HandlerFunc(api.DeleteChEditor)
+
+	// Views by Editors
+	writerRouter.Methods("GET").Path("/view/cheditor/{link_url}").HandlerFunc(api.GetSingleChWriterJSON)
+
+	// Accounts SubRouter
+	accountsRouter := r.PathPrefix("/accounts").Subrouter()
+
+	accountsRouter.Methods("POST").Path("/get_user_info").HandlerFunc(api.GetUserInfo)
+	accountsRouter.Methods("GET").Path("/oauth/login_url").HandlerFunc(api.GetLoginURLJson)
+	accountsRouter.Methods("GET").Path("/oauth/{my_type}/callback").HandlerFunc(api.Authenticate)
+	accountsRouter.Methods("POST").Path("/logout").HandlerFunc(api.LogoutSession)
+
+	//headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins(allowedOrigins)
+	credentialsOk := handlers.AllowCredentials()
+
+	server := handlers.CORS(originsOk, credentialsOk)(r)
+
+	log.Println("starting...")
+
+	if loads.ElDevelopment {
+		err := http.ListenAndServe(Port, server)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	} else {
+		err := http.ListenAndServeTLS(Port, Cert, Key, server)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}
+
+
+}
